6026eb5abc67f8873be0bcf9fba13094bf86170f
benito lsi <maryam_l89@yahoo.com>
2022-09-19 16:01:51 +0430

    Add project files.

---
+++ Petshop.Core/Categories/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Petshop.Core.Categories
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Petshop.Core/Products/Product.cs
@@ -0,0 +1,15 @@
+﻿using Petshop.Core.Categories;
+
+namespace Petshop.Core.Products
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
