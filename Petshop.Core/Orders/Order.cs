03cc5be99fc9038309f145926430c5bad0657814
benito lsi <maryam_l89@yahoo.com>
2022-11-28 13:11:22 +0330

    add-fontawsome-to-root

---
+++ Petshop.Core/Orders/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Petshop.Core.Orders
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public List<OrderInfo> Orders { get; set; }
+    }
+}
---
+++ Petshop.Core/Orders/OrderInfo.cs
@@ -0,0 +1,11 @@
+﻿using Petshop.Core.Products;
+
+namespace Petshop.Core.Orders
+{
+    public class OrderInfo
+    {
+        public int Id { get; set; }
+        public Product Products { get; set; }
+        public int Quantity { get; set; }
+    }
+}
